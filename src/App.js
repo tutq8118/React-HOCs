e71e9710e1db06ccf5956934abcd35cdf23a122f
Glitch (magic-olive-muscle) <none>
2020-07-24 01:55:47 +0000

    💎👝 Updated with Glitch

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import withFadeInWhenInView from './components/withFadeInWhenInView.js';
+
+const FadingIn = withFadeInWhenInView(() => <h1>I'm fading innnnn...</h1>);
+                                      
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code>
+          </p>
+          <a
+            className="App-link"
+            href="https://coders-x.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hãy cuộn xuống dưới ở màn hình kết quả<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+            Yêu anh Thịnh<br />
+          </a>
+          <FadingIn />
+        </header>      
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/withFadeInWhenInView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const withFadeInWhenInView = (WrappedComponent) => {
+  return 'not implemented yet';
+  // Viết code ở đây sao cho khi người dùng cuộn chuột trên trang,
+  // và WrappedComponent đi từ ngoài vào trong cửa sổ trình duyệt 
+  // thì nó sẽ hiện ra từ từ với animation speed là 2s (hiệu ứng fadein)
+}
+
+export default withFadeInWhenInView;
